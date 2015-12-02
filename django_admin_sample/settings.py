33533d563be9370cb535f241f9bf6a1062e9fe2f
jeonghwan <ej88ej@gmail.com>
2015-12-02 19:32:45 +0900

    d

--- django_admin_sample/settings.py
+++ django_admin_sample/settings.py
@@ -77,10 +77,6 @@ WSGI_APPLICATION = 'django_admin_sample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'HOST': 'localhost',
